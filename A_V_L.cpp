adf062730b8689c2f4a395e9cf154893338b3f24
Md kaif khan <35622849+kaif3007@users.noreply.github.com>
2018-10-02 15:54:06 +0530

    Add files via upload

---
+++ A_V_L.cpp
@@ -0,0 +1,195 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+ struct node
+ {  
+ 	int data;
+ 	int height;
+ 	node* left; 
+ 	node* right;
+ };
+
+node* makenode(int data)
+{
+	node*temp=new node;
+	temp->data=data;
+	temp->left=NULL;
+	temp->right=NULL;
+	temp->height=1; 
+	return temp;
+}
+
+
+int height(node*n)
+{
+	if(n==NULL)
+	return 0;
+	
+	return n->height;
+}
+
+int balance(node*n)
+{
+	return (height(n->left)-height(n->right));
+}
+
+node* rightrotate(node* root)
+{
+	node*x=root->left;
+	node*y=x->right;
+	
+	x->right=root;                                                    //rightrotation algo;
+	root->left=y;
+	
+	
+	root->height=1+max(height(root->left),height(root->right));
+	x->height=1+max(height(x->left),height(x->right));
+	
+	return x;
+}
+
+node* leftrotate(node* root)
+{
+	node*x=root->right;
+	node*y=x->left;
+	
+	x->left=root;
+	root->right=y;
+	
+	                                                                                    //leftrotation algo
+	root->height=1+max(height(root->left),height(root->right));
+	x->height=1+max(height(x->left),height(x->right));
+	
+	return x;
+}
+
+node *avl(node*root,int data)
+{
+	if(root==NULL)
+	return makenode(data);
+	
+	if(root->data>data)
+	root->left=avl(root->left,data);
+	
+	if(root->data<data)
+	root->right=avl(root->right,data);
+	
+	if(root->data==data)
+	return root;
+	
+	root->height=1+max(height(root->left),height(root->right));
+	
+	int b=balance(root);
+	
+	if(b>1 && data<root->left->data)
+	return rightrotate(root);
+	if(b<-1 && data>root->right->data)
+	return leftrotate(root);
+	if(b>1 && data>root->left->data)
+	{
+		root->left=leftrotate(root->left);
+		return rightrotate(root);                                  //four different cases ll rr lr rl;
+	}
+	if(b<-1 && data<root->right->data)
+	{
+		root->right=rightrotate(root->right);
+		return leftrotate(root);
+	}
+	return root;
+}
+
+
+node* deleteavl(node* head,int data)
+{if(head==NULL)
+return head;
+
+if	(head->data>data)
+head->left=deleteavl(head->left,data);
+
+if(head->data<data)
+head->right=deleteavl(head->right,data);
+
+if(head->data==data)
+{
+  if(head->right==NULL)
+  { node*temp=head->left;
+  free(head);
+  return temp;
+	}
+	
+	else if(head->left==NULL)
+  { node*temp=head->right;
+  free(head);
+  return temp;
+	 }
+	 
+	 else
+	 {node*temp=head->right;
+	 	while(temp->left!=NULL)
+	 	temp=temp->left;
+	 	
+	 	head->data=temp->data;
+	 	head->right=deleteavl(head->right,temp->data);
+	 }
+	 
+	 if(head==NULL)
+       return head;	 
+       
+
+	 	head->height=1+max(height(head->left),height(head->right));
+}
+	int b=balance(head);
+	
+	if(b>1 && data<head->left->data)
+	return rightrotate(head);
+	if(b<-1 && data>head->right->data)
+	return leftrotate(head);
+	if(b>1 && data>head->left->data)
+	{
+		head->left=leftrotate(head->left);
+		return rightrotate(head);                                  //four different cases ll rr lr rl;
+	}
+	if(b<-1 && data<head->right->data)
+	{
+		head->right=rightrotate(head->right);
+		return leftrotate(head);
+	}
+	
+	
+	return head;
+}
+
+
+void inorder(node*head)
+{
+	if(head==NULL)
+	return;
+	else
+	{
+		inorder(head->left);
+		cout<<head->data<<" ";                                                       
+		inorder(head->right);
+	}
+}
+
+
+int main() 
+{ node *root = NULL;
+ 
+    root = avl(root, 9);
+    root = avl(root, 5);
+    root = avl(root, 10);
+    root = avl(root, 0);
+    root = avl(root, 6);
+    root = avl(root, 11);
+    root = avl(root, -1);
+    root = avl(root, 1);
+    root = avl(root, 2);
+    
+    
+    //root=deleteavl(root,1);
+     inorder(root);
+ 
+ 
+	return 0;
+}
---
+++ red_black trees.cpp
@@ -0,0 +1,195 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+	int data;
+	node* left;
+	node* right;
+	node* parent;
+	bool colour;
+};
+
+node* createnode(int dat)
+{
+node* temp=new node;
+temp->data=dat;
+temp->left=NULL;
+temp->right=NULL;
+temp->parent=NULL;	
+temp->colour=0;
+return temp;
+}
+
+node* insert(node* root,node* &p)
+{
+	if(root==NULL)
+	return p;
+	
+	else if(root->data>p->data)
+	{root->left=insert(root->left,p);
+	root->left->parent=root;
+    }
+    
+    else if(root->data<=p->data)
+    {root->right=insert(root->right,p);
+	root->right->parent=root;
+    }
+    
+    return root;
+}
+
+void rightrot(node* &root,node* &pt)
+{
+	node* temp=pt->left;
+	pt->left=temp->right;
+	
+	if(pt->left!=NULL)
+	pt->left->parent=pt;
+	
+	if(pt->parent==NULL)
+	root=temp;
+	
+	else if(pt==pt->parent->left)
+	pt->parent->left=temp;
+	
+	else
+	pt->parent->right=temp;
+	
+	temp->right=pt;
+	pt->parent=temp;
+}
+
+
+void leftrot(node* &root,node* &pt)
+{
+	node* temp=pt->right;
+	pt->right=temp->left;
+	
+	if(pt->right!=NULL)
+	pt->right->parent=pt;
+	
+	if(pt->parent==NULL)
+	root=temp;
+	
+	else if(pt==pt->parent->right)
+	pt->parent->right=temp;
+	
+	else
+	pt->parent->left=temp;
+	
+	temp->left=pt;
+	pt->parent=temp;
+}
+
+void fixviolation(node* &root,node* &p)
+{
+	node* papa=NULL;
+	node* dada=NULL;
+	while((p!=root)&&(p->colour!=1)&&(p->parent->colour==0))
+	{
+		papa=p->parent;
+		dada=p->parent->parent;
+		if(papa==dada->left)
+		{
+			node* uncle=dada->right;
+			if(uncle!=NULL && uncle->colour==0)
+			{
+				dada->colour=0;
+				papa->colour=1;
+				uncle->colour=1;
+				p=dada;
+			}
+			else if(p==papa->right)
+			{
+				leftrot(root,papa);
+				p=papa;
+				papa=p->parent;
+			}
+			else if(p==papa->left)
+			{
+				rightrot(root,dada);
+				if(dada->colour==0)
+				dada->colour=1;
+				else
+				dada->colour=0;
+				
+				if(papa->colour==0)
+				papa->colour=1;
+				else
+				papa->colour=0;
+				p=papa;
+			}
+		}
+		
+		else
+		{
+			node* uncle=dada->left;
+			if(uncle!=NULL && uncle->colour==0)
+			{
+				dada->colour=0;
+				papa->colour=1;
+				uncle->colour=1;
+				p=dada;
+			}
+			else if(p==papa->left)
+			{
+				rightrot(root,papa);
+				p=papa;
+				papa=p->parent;
+			}
+			else if(p==papa->right)
+			{
+				leftrot(root,dada);
+				if(dada->colour==0)
+				dada->colour=1;
+				else
+				dada->colour=0;
+				
+				if(papa->colour==0)
+				papa->colour=1;
+				else
+				papa->colour=0;
+				p=papa;
+			}
+		}
+	}
+	root->colour=1;
+}
+
+void insertrb(node* &root,int dat)
+{
+	node* temp=createnode(dat);
+	root=insert(root,temp);
+	
+	fixviolation(root,temp);
+}
+
+void inorder(node* root)
+{
+	if(root==NULL)
+	return;
+	inorder(root->left);
+	cout<<root->data<<"_"<<root->colour<<" ";
+	inorder(root->right);
+}
+
+
+int main()
+{node* root=NULL;
+insertrb(root,9);
+insertrb(root,5);
+insertrb(root,10);
+insertrb(root,0);
+insertrb(root,6);
+insertrb(root,11);
+insertrb(root,11);
+insertrb(root,-1);
+insertrb(root,1);
+insertrb(root,2);
+
+
+inorder(root);
+	
+	return 0;
+}
